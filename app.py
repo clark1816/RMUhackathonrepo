24b438a3a5716a3312d7abc57ea0df0144954e85
clark1816 <115838844+letstradebot@users.noreply.github.com>
2022-11-06 10:42:42 -0500

    added more to option Rergreesion all data
    added way for user to test inputs

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas
 from sklearn import linear_model
 
 #,,,,,,liquid level [m],rotate speed [rpm],,,outlet A wt%,outlet B wt%
-option = st.sidebar.selectbox("Which Dashboard?", ('home', 'Linear Regression All Data','Linear Regression Correlation Data', 'Hybrid'),0)
+option = st.sidebar.selectbox("Which Dashboard?", ('home', 'Linear Regression All Data', 'Hybrid'),0)
 if option == 'home':
     st.write('ROMOs Gold the Gold Standard in Adventuring')
 
@@ -28,8 +28,19 @@ if option == 'Linear Regression All Data':
 
     
     if value_9:
-        outletpolym = (-2.5620332e-04*value_1) + (7.8522271e-01*value_2) + (8.2543677e-01*value_3) + (3.5442629e+00*value_4) + (3.2703043e-03*value_5) + (-1.3718221e+02*value_6) + (-7.6586172e-02*value_7) + (2.4416444e-03*value_8) + (2.9435358e-03*value_9)
-        st.write(f'Write answer: {outletpolym}')
+        Poly_value = (-2.45598028e-04*value_1) + (7.99089074e-01*value_2) + (8.35692048e-01*value_3) + (3.33927655*value_4) + (3.24872392e-03*value_5) + (-1.59723953e+02*value_6) + (-1.03581265e-01*value_7) + (2.27641081e-03
+*value_8) + (3.19753168e-03*value_9) +-1.3887262344360352
+        
+        b_value = (2.5583830e-04*value_1) + (1.9988307e-01*value_2) + (1.3920839e-01*value_3) + (-3.6337156*value_4) + ( -1.5648461e-03*value_5) + (1.4676350e+02*value_6) + (9.1752745e-02*value_7) + (-2.5921480e-03
+*value_8) + (-3.2897484e-03*value_9) +2.2742693424224854
+        
+        a_value = Poly_value - b_value
+        
+        st.write(f'Total Polymer %: {Poly_value}')
+        st.write(f'Polymer A% %: {a_value}')
+        st.write(f'Polymer A% %: {b_value}')
+        
+        
 
 if option == 'Linear Regression Correlation Data':
     st.write('option 2')
@@ -62,6 +73,29 @@ if option == 'Linear Regression Correlation Data':
         st.write(f'total predicted polymer % is {total_poly}')
         
 
+if option == 'Hybrid':
+    value_1 = st.number_input('inlet feed [kg/h]')
+
+    value_2 = st.number_input('inlet polymer wt%')
+
+    value_3 = st.number_input('inlet A wt%')
+
+    value_4 = st.number_input('inlet B wt%')
+
+    value_5 = st.number_input('inlet temp [degC]')
+
+    value_6 = st.number_input('pressure [MPa]')
+
+    value_7 = st.number_input('liquid level [m]')
+
+    value_8 = st.number_input('rotate speed [rpm]')
+
+    value_9 = st.number_input('bottom temp [degC]')
+
+    
+    if value_9:
+        outletpolym = (-2.5620332e-04*value_1) + (7.8522271e-01*value_2) + (8.2543677e-01*value_3) + (3.5442629e+00*value_4) + (3.2703043e-03*value_5) + (-1.3718221e+02*value_6) + (-7.6586172e-02*value_7) + (2.4416444e-03*value_8) + (2.9435358e-03*value_9)
+        st.write(f'Write answer: {outletpolym}')
 
 
 
@@ -74,3 +108,5 @@ if option == 'Linear Regression Correlation Data':
 
 
 print('code completed')
+
+
